c8f150ef52b7d89fef21ff3d4ff41bc957fef799
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-30 19:00:19 +0530

    context api

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
-import { useState, useCallback } from "react";
-import "./App.css";
-
-
-function Child({handleClick}){
-  
-  // console.log("the child has been rendered")
-
-  return(
-    <div>
-        <button onClick={handleClick}>Click Me</button>
-        
-    </div>
-  )
-}
+import React,{useContext} from "react";
+import { CounterContext } from "./context/counterContext";
 
 function App() {
-  // const [num, setNum] = useState(0);
-  const [other,setOther]=useState(0);
-
-  console.log("the parent has been rendered")
-
-  const handleClick=useCallback(()=>{
-            console.log("the child has been rendered") 
-  },[])
   
+   const state=useContext(CounterContext)
+
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div>
-       <h1>Other Num:{other}</h1>
-       <button onClick={()=>setOther(other+1)}>Change Other</button>
-       <Child handleClick={handleClick}/>
-      </div>
+
+    <div>
+      
+       <h1>{state.counter}</h1>
+       <button onClick={()=>state.setCounter((prev)=>prev+1)}>Change counter</button>    
     </div>
   );
 }
