b414dc63dd123b1e18012101a327042577dd8d67
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-30 11:29:28 +0530

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { useState,useEffect } from 'react';
+import Component from "./components/Component"
 
 function App() {
+
+  const [data,setData]=useState(0)
+
+  const handleClick=()=>{
+    setData(data+1);
+  }
+
   return (
-    <div>
-        <h1>hello world</h1> 
-        {/* functional componenet */}
+    <div style={{height:"100vh",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+        <h1>{data} is the number of data points</h1>
+        <button style={{height:"50px",width:"100px",borderRadius:"5px",backgroundColor:"black",color:"white"}} onClick={handleClick}>Click</button>
+       
     </div>
   );
 }
