876091278c777659ada0230d717eea135fe47a44
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-30 11:43:54 +0530

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import './App.css';
-import { useState,useEffect } from 'react';
-import Component from "./components/Component"
+import { useState,useEffect, use } from 'react';
 
 function App() {
 
-  const [data,setData]=useState(0)
-
-  const handleClick=()=>{
-    setData(data+1);
-  }
+   const [data,setData]=useState({})
+   useEffect(()=>{
+       fetch("https://jsonplaceholder.typicode.com/users/2")
+       .then(res=>res.json())
+       .then(data=>setData(data))
+       .catch(err=>console.log(err))
+   },[])
 
   return (
     <div style={{height:"100vh",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
-        <h1>{data} is the number of data points</h1>
-        <button style={{height:"50px",width:"100px",borderRadius:"5px",backgroundColor:"black",color:"white"}} onClick={handleClick}>Click</button>
+        
+        {data?(
+
+           <div>
+            <h1>{data.name}</h1>
+            <p>{data.phone}</p>
+             <p>{data.email}</p>
+             <p>{data.website}</p>
+           </div>
+
+        ):(
+           <p>data is loading...</p>
+        )}
        
     </div>
   );
