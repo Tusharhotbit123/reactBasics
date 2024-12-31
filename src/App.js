f570c08ce42fe2b79b907a7e54771cc305e4b43c
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-31 12:36:17 +0530

    dynamic routing example

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React,{useContext} from "react";
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import UserDetails from "./components/UserDetails";
+import Users from "./components/Users";
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
         <Routes>
            <Route path="/" element={<Home/>}/>
            <Route path="/about" element={<About/>}/>
+           <Route path="/users" element={<Users/>}/>
+           <Route path="/users/:id" element={<UserDetails/>} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/UserDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserDetails = () => {
+  return (
+    <div>
+      <p>this is the user details page</p>      
+    </div>
+  )
+}
+
+export default UserDetails
