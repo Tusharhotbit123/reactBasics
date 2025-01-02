e2d81b3085e890efd587573b11e85296c34cdb5b
Tushar Sharma <tsharma.hotbitinfosoft.com>
2025-01-02 14:45:07 +0530

    use of key in reactjs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import UserDetails from "./components/UserDetails";
-import Users from "./components/Users";
-import Navbar from "./components/Navbar";
 import PageNotFound from "./components/PageNotFound";
-import CountSearch from "./components/CountSearch";
-import Protected from "./components/Protected";
-import Login from "./components/Login";
+import UserCard from "./components/UserCard";
 
 function App() {
 
@@ -16,15 +9,9 @@ function App() {
 
     <div>
       <Router>
-           {/* <Navbar/> */}
         <Routes>
-           <Route path="/login" element={<Login/>}/>
-           <Route path="/home" element={<Protected Component={Home}/>}/>
-           <Route path="/about" element={<Protected Component={About}/>}/>
-           <Route path="/users" element={<Users/>}/>
-           <Route path="/users/:id/:category" element={<UserDetails/>} />
-           <Route path="/search" element={<CountSearch/>}/>
            <Route path="*" element={<PageNotFound/>}/>
+           <Route path="/users" element={<UserCard/>}/>
         </Routes>
       </Router>
     </div>
