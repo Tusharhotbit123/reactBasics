8f52c855bdd32af54a661f271e77d1de2bc88d3b
Tushar Sharma <tsharma.hotbitinfosoft.com>
2025-01-02 16:28:58 +0530

    array methods in reactjs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import PageNotFound from "./components/PageNotFound";
 import UserCard from "./components/UserCard";
 import Form from "./components/Form";
+import ArrayMethods from "./components/ArrayMethods";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
            <Route path="*" element={<PageNotFound/>}/>
            <Route path="/users" element={<UserCard/>}/>
            <Route path="/form" element={<Form/>}/>
+           <Route path="/methods" element={<ArrayMethods/>}/>
         </Routes>
       </Router>
     </div>
